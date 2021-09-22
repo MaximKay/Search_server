b9fa5838d886288d1bdbaa2f24dcd7d0008a176c
MaximKay <89381518+MaximKay@users.noreply.github.com>
2021-09-22 16:07:00 +0300

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -384,8 +384,7 @@ void CustomFiltersTests() {
 		SearchServer server;
 		server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
 		server.AddDocument(doc_id_2, content_2, DocumentStatus::BANNED, ratings_2);
-		const auto filter = [](int document_id, DocumentStatus document_status, int rating)
-																		{return document_status == DocumentStatus::BANNED;};
+		const auto filter = [](int document_id, DocumentStatus document_status, int rating){return document_status == DocumentStatus::BANNED;};
 		const auto found_docs = server.FindTopDocuments("in"s, filter);
 		ASSERT_EQUAL_HINT(found_docs.size(), 1u, "Status filter found wrong amount of documents"s);
 		ASSERT_EQUAL_HINT(found_docs[0].id, 12, "Status filter found wrong document"s);
@@ -394,8 +393,7 @@ void CustomFiltersTests() {
 		SearchServer server;
 		server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
 		server.AddDocument(doc_id_2, content_2, DocumentStatus::ACTUAL, ratings_2);
-		const auto filter = [](int document_id, DocumentStatus document_status, int rating)
-																				{return rating > 3;};
+		const auto filter = [](int document_id, DocumentStatus document_status, int rating){return rating > 3;};
 		const auto found_docs = server.FindTopDocuments("in"s, filter);
 		ASSERT_EQUAL_HINT(found_docs.size(), 1u, "Rating filter found wrong amount of documents"s);
 		ASSERT_EQUAL_HINT(found_docs[0].id, 12, "Rating filter found wrong document"s);
@@ -404,8 +402,7 @@ void CustomFiltersTests() {
 		SearchServer server;
 		server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
 		server.AddDocument(doc_id_2, content_2, DocumentStatus::ACTUAL, ratings_2);
-		const auto filter = [](int document_id, DocumentStatus document_status, int rating)
-																					{return document_id > 20;};
+		const auto filter = [](int document_id, DocumentStatus document_status, int rating){return document_id > 20;};
 		const auto found_docs = server.FindTopDocuments("in"s, filter);
 		ASSERT_EQUAL_HINT(found_docs.size(), 1u, "Id filter found wrong amount of documents"s);
 		ASSERT_EQUAL_HINT(found_docs[0].id, 42, "Id filter found wrong document"s);
